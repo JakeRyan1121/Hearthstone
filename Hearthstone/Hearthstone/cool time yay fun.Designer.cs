92d1f1f37eb64c29f74411bd19b2e3c9151beefd
Joshua Howard <jrhoward@students.wcpss.net>
2019-05-17 08:24:49 -0400

    cool time yay

---
+++ Hearthstone/Hearthstone/cool time yay fun.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Hearthstone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDeck1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblDeck2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDeck1
+            // 
+            this.lblDeck1.AutoSize = true;
+            this.lblDeck1.Location = new System.Drawing.Point(42, 31);
+            this.lblDeck1.Name = "lblDeck1";
+            this.lblDeck1.Size = new System.Drawing.Size(35, 13);
+            this.lblDeck1.TabIndex = 2;
+            this.lblDeck1.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(333, 31);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblDeck2
+            // 
+            this.lblDeck2.AutoSize = true;
+            this.lblDeck2.Location = new System.Drawing.Point(649, 36);
+            this.lblDeck2.Name = "lblDeck2";
+            this.lblDeck2.Size = new System.Drawing.Size(35, 13);
+            this.lblDeck2.TabIndex = 4;
+            this.lblDeck2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDeck2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDeck1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDeck1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblDeck2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hearthstone/Hearthstone/cool time yay fun.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hearthstone
+{
+    public partial class Form1 : Form
+    {
+        Card[] arrayAllCards = new Card[30];//an array holding all the spells
+        Card[] arrayP1Deck = new Card[30];//an array holding the player 1 deck
+        Card[] arrayP2Deck = new Card[30];//an array holding player 2 deck
+
+        Random randomCard = new Random();
+
+        List<Card> lstPlayer1 = new List<Card>();
+        List<Card> lstPlayer2 = new List<Card>();
+
+        List<Card> lstP1Hand = new List<Card>();
+        List<Card> lstP2Hand = new List<Card>();
+
+        
+
+        int intCounter = 0;//declare counter for the loop
+        //declaring variables for the inside of the loop
+        int int1stHalf = 0;
+        int int2ndHalf = 0;
+        int intAttackHolder = 0;
+        int intManaCostHolder = 0;
+        int intHealthHolder = 0;
+        string strNameHolder = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartUp()
+        {
+            for (int i = 0; i < 30; i++)//loop to instanceiate the array of all spells
+            {
+                arrayAllCards[i] = new Card();
+                arrayP1Deck[i] = new Card();
+                arrayP2Deck[i] = new Card();
+
+            }
+
+            arrayAllCards[0].Name = "Minion 1";
+            arrayAllCards[1].Name = "Minion 2";
+            arrayAllCards[2].Name = "Minion 3";
+            arrayAllCards[3].Name = "Minion 4";
+            arrayAllCards[4].Name = "Minion 5";
+            arrayAllCards[5].Name = "Minion 6";
+            arrayAllCards[6].Name = "Minion 7";
+            arrayAllCards[7].Name = "Minion 8";
+            arrayAllCards[8].Name = "Minion 9";
+            arrayAllCards[9].Name = "Minion 10";
+            arrayAllCards[10].Name = "Minion 11";
+            arrayAllCards[11].Name = "Minion 12";
+            arrayAllCards[12].Name = "Minion 13";
+            arrayAllCards[13].Name = "Minion 14";
+            arrayAllCards[14].Name = "Minion 15";
+            arrayAllCards[15].Name = "Minion 16";
+            arrayAllCards[16].Name = "Minion 17";
+            arrayAllCards[17].Name = "Minion 18";
+            arrayAllCards[18].Name = "Minion 19";
+            arrayAllCards[19].Name = "Minion 20";
+            arrayAllCards[20].Name = "Minion 21";
+            arrayAllCards[21].Name = "Minion 22";
+            arrayAllCards[22].Name = "Minion 23";
+            arrayAllCards[23].Name = "Minion 24";
+            arrayAllCards[24].Name = "Minion 25";
+            arrayAllCards[25].Name = "Minion 26";
+            arrayAllCards[26].Name = "Minion 27";
+            arrayAllCards[27].Name = "Minion 28";
+            arrayAllCards[28].Name = "Minion 29";
+            arrayAllCards[29].Name = "Minion 30";
+
+
+            arrayAllCards[0].Attack = 1;
+            arrayAllCards[1].Attack = 1;
+            arrayAllCards[2].Attack = 1;
+            arrayAllCards[3].Attack = 1;
+            arrayAllCards[4].Attack = 1;
+            arrayAllCards[5].Attack = 1;
+            arrayAllCards[6].Attack = 1;
+            arrayAllCards[7].Attack = 1;
+            arrayAllCards[8].Attack = 1;
+            arrayAllCards[9].Attack = 1;
+            arrayAllCards[10].Attack = 1;
+            arrayAllCards[11].Attack = 1;
+            arrayAllCards[12].Attack = 1;
+            arrayAllCards[13].Attack = 1;
+            arrayAllCards[14].Attack = 1;
+            arrayAllCards[15].Attack = 1;
+            arrayAllCards[16].Attack = 1;
+            arrayAllCards[17].Attack = 1;
+            arrayAllCards[18].Attack = 1;
+            arrayAllCards[19].Attack = 1;
+            arrayAllCards[20].Attack = 1;
+            arrayAllCards[21].Attack = 1;
+            arrayAllCards[22].Attack = 1;
+            arrayAllCards[23].Attack = 1;
+            arrayAllCards[24].Attack = 1;
+            arrayAllCards[25].Attack = 1;
+            arrayAllCards[26].Attack = 1;
+            arrayAllCards[27].Attack = 1;
+            arrayAllCards[28].Attack = 1;
+            arrayAllCards[29].Attack = 1;
+
+
+            arrayAllCards[0].Health = 1;
+            arrayAllCards[1].Health = 1;
+            arrayAllCards[2].Health = 1;
+            arrayAllCards[3].Health = 1;
+            arrayAllCards[4].Health = 1;
+            arrayAllCards[5].Health = 1;
+            arrayAllCards[6].Health = 1;
+            arrayAllCards[7].Health = 1;
+            arrayAllCards[8].Health = 1;
+            arrayAllCards[9].Health = 1;
+            arrayAllCards[10].Health = 1;
+            arrayAllCards[11].Health = 1;
+            arrayAllCards[12].Health = 1;
+            arrayAllCards[13].Health = 1;
+            arrayAllCards[14].Health = 1;
+            arrayAllCards[15].Health = 1;
+            arrayAllCards[16].Health = 1;
+            arrayAllCards[17].Health = 1;
+            arrayAllCards[18].Health = 1;
+            arrayAllCards[19].Health = 1;
+            arrayAllCards[20].Health = 1;
+            arrayAllCards[21].Health = 1;
+            arrayAllCards[22].Health = 1;
+            arrayAllCards[23].Health = 1;
+            arrayAllCards[24].Health = 1;
+            arrayAllCards[25].Health = 1;
+            arrayAllCards[26].Health = 1;
+            arrayAllCards[27].Health = 1;
+            arrayAllCards[28].Health = 1;
+            arrayAllCards[29].Health = 1;
+
+
+            arrayAllCards[0].ManaCost = 1;
+            arrayAllCards[1].ManaCost = 1;
+            arrayAllCards[2].ManaCost = 1;
+            arrayAllCards[3].ManaCost = 1;
+            arrayAllCards[4].ManaCost = 1;
+            arrayAllCards[5].ManaCost = 1;
+            arrayAllCards[6].ManaCost = 1;
+            arrayAllCards[7].ManaCost = 1;
+            arrayAllCards[8].ManaCost = 1;
+            arrayAllCards[9].ManaCost = 1;
+            arrayAllCards[10].ManaCost = 1;
+            arrayAllCards[11].ManaCost = 1;
+            arrayAllCards[12].ManaCost = 1;
+            arrayAllCards[13].ManaCost = 1;
+            arrayAllCards[14].ManaCost = 1;
+            arrayAllCards[15].ManaCost = 1;
+            arrayAllCards[16].ManaCost = 1;
+            arrayAllCards[17].ManaCost = 1;
+            arrayAllCards[18].ManaCost = 1;
+            arrayAllCards[19].ManaCost = 1;
+            arrayAllCards[20].ManaCost = 1;
+            arrayAllCards[21].ManaCost = 1;
+            arrayAllCards[22].ManaCost = 1;
+            arrayAllCards[23].ManaCost = 1;
+            arrayAllCards[24].ManaCost = 1;
+            arrayAllCards[25].ManaCost = 1;
+            arrayAllCards[26].ManaCost = 1;
+            arrayAllCards[27].ManaCost = 1;
+            arrayAllCards[28].ManaCost = 1;
+            arrayAllCards[29].ManaCost = 1;
+
+           
+            //lstPlayer1.AddRange(arrayAllCards);
+            
+        }
+        public void DeckShuffle()
+        {
+            while (intCounter < 30)//shuffle loop
+            {
+                //STEP 1: METAPHORICALLY SPLITING THE DECK
+                int1stHalf = randomCard.Next(0, 15);//random in first half
+                int2ndHalf = randomCard.Next(15, 30);//random in second half
+
+                //STEP 2: SETTING ASIDE/ SAVING 1ST HALF VALUE
+                intAttackHolder = arrayAllCards[int1stHalf].Attack;
+                intManaCostHolder = arrayAllCards[int1stHalf].ManaCost;
+                intHealthHolder = arrayAllCards[int1stHalf].Health;
+                strNameHolder = arrayAllCards[int1stHalf].Name;
+                //test message
+
+                //STEP 3: REPLACING 1ST HALF WITH 2ND HALF
+                arrayAllCards[int1stHalf].Attack = arrayAllCards[int2ndHalf].Attack;
+                arrayAllCards[int1stHalf].ManaCost = arrayAllCards[int2ndHalf].ManaCost;
+                arrayAllCards[int1stHalf].Health = arrayAllCards[int2ndHalf].Health;
+                arrayAllCards[int1stHalf].Name = arrayAllCards[int2ndHalf].Name;
+
+                //STEP 4: FILLING IN THE 2ND HALF WITH THE SAVED VALUES
+                arrayAllCards[int2ndHalf].Attack = intAttackHolder;
+                arrayAllCards[int2ndHalf].ManaCost = intManaCostHolder;
+                arrayAllCards[int2ndHalf].Health = intHealthHolder;
+                arrayAllCards[int2ndHalf].Name = strNameHolder;
+                //test message after switch
+                //MessageBox.Show(arrayCards[int1stHalf].Value + " " + arrayCards[int1stHalf].Suit + "\n" + arrayCards[int2ndHalf].Value + " " + arrayCards[int2ndHalf].Suit + "\n" + intValueHolder + " " + strSuitHolder);
+
+                intCounter++;//add to counter
+            }
+            intCounter = 0;
+            
+        }
+        
+        public void Player1List()
+        {
+            DeckShuffle();
+
+            for (int i = 0; i < 30; i++)
+            {
+                arrayP1Deck[i].Name = arrayAllCards[i].Name;
+                arrayP1Deck[i].Attack = arrayAllCards[i].Attack;
+                arrayP1Deck[i].Health = arrayAllCards[i].Health;
+                arrayP1Deck[i].ManaCost = arrayAllCards[i].ManaCost;
+                arrayP1Deck[i].Effect = arrayAllCards[i].Effect;
+            }
+
+            lstPlayer1.AddRange(arrayP1Deck);
+
+            for (int i = 0; i < 30; i++)
+            {
+                lblDeck1.Text += lstPlayer1[i].Name + "\n";
+            }
+        }
+        public void Player2List()
+        {
+            DeckShuffle();
+
+            for (int i = 0; i < 30; i++)
+            {
+                arrayP2Deck[i].Name = arrayAllCards[i].Name;
+                arrayP2Deck[i].Attack = arrayAllCards[i].Attack;
+                arrayP2Deck[i].Health = arrayAllCards[i].Health;
+                arrayP2Deck[i].ManaCost = arrayAllCards[i].ManaCost;
+                arrayP2Deck[i].Effect = arrayAllCards[i].Effect;
+            }
+
+            lstPlayer2.AddRange(arrayP2Deck);
+
+            for (int i = 0; i < 30; i++)
+            { 
+                lblDeck2.Text += lstPlayer2[i].Name + "\n";
+            }
+        }
+
+        public void DrawCard()
+        {
+            var intEndOfList = 0;
+            int intCountHand = 0;
+
+            intEndOfList = lstPlayer1.Count - 1;
+            MessageBox.Show(Convert.ToString(intEndOfList));
+            lstP1Hand[intCountHand].Name = lstPlayer1[intEndOfList - 1].Name;
+            lstP1Hand[intCountHand].Attack = lstPlayer1[intEndOfList - 1].Attack;
+            lstP1Hand[intCountHand].Health = lstPlayer1[intEndOfList].Health;
+            lstP1Hand[intCountHand].ManaCost = lstPlayer1[intEndOfList].ManaCost;
+            lstP1Hand[intCountHand].Effect = lstPlayer1[intEndOfList].Effect;
+
+            lstPlayer1.RemoveAt(intEndOfList);
+
+            intCountHand++;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartUp();
+            Player1List();
+            Player2List();
+
+            DrawCard();
+        }
+    }
+
+    public class Card
+    {
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+        public int ManaCost { get; set; }
+        public bool Effect { get; set; } //fix this
+    }
+}
+
+    
+
